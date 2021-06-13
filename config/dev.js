5276e0932af801e02bfb733781f7f16549f82042
c4miloruiz <cgomez08@gmail.com>
2021-06-13 13:12:08 -0500

    Travis config

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://admin:w7RCT-XRS9Y7TLa@dev-shard-00-00.nt80x.mongodb.net:27017,dev-shard-00-01.nt80x.mongodb.net:27017,dev-shard-00-02.nt80x.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-ub31e8-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisURL: 'redis://127.0.0.1:6379'
+};
---
+++ tests/header.test.js
@@ -0,0 +1,30 @@
+const Page = require('./helpers/page')
+
+let page
+
+beforeEach(async () => {
+  page = await Page.build()
+  await page.goto('http://localhost:3000')
+})
+
+afterEach(async () => {
+  await page.close()
+})
+
+test('Header has the correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo')
+  expect(text).toEqual('Blogster')
+})
+
+test('Click login start oAouth flow', async () => {
+  const text = await page.click('.right a')
+  const url = await page.url()
+  expect(url).toMatch(/accounts\.google\.com/)
+})
+
+test('When senged in, show logaou button', async () => {
+  await page.login()
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+  expect(text).toEqual('Logout')
+})
+
